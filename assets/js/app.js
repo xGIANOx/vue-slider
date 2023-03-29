eca23c0dd012dae22dceccfbcdc76652cee367e0
xGIANOx <studioenricogian@gmail.com>
2023-03-29 16:40:54 +0200

    [add] slider,infinite loop

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,30 +1,34 @@
 
 const { createApp } = Vue
-  createApp({
+
+createApp({
     data() {
       return {
-        movie : {
-            image : [
-                "./assets/img/01.webp",
-                "./assets/img/02.webp",
-            ]
-                
-        }
-      }
-    },
-    methods : {
-        next(){
-            activeImage++
-            console.log('next cliccato')
-            console.log(activeImage); 
+        movie: {
+          image: [
+            "./assets/img/01.webp",
+            "./assets/img/02.webp",
+            "./assets/img/03.webp",
+            "./assets/img/04.webp",
+            "./assets/img/05.webp",
+          ],
         },
-        prev(){
-            activeImage--
-            console.log('prev cliccato')
-            console.log(activeImage); 
+        activeImage: 0,
+      };
+    },
+    methods: {
+      next() {
+        this.activeImage++;
+        if (this.activeImage === this.movie.image.length) {
+          this.activeImage = 0;
         }
-
-    }
-    
-
-}).mount('#app')
+      },
+      prev() {
+        this.activeImage--;
+        if (this.activeImage < 0) {
+          this.activeImage = this.movie.image.length - 1;
+        }
+      },
+    },
+  }).mount("#app");
+  
