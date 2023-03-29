93853e7bcd4bbac52267452c5edaae6f018d802a
xGIANOx <studioenricogian@gmail.com>
2023-03-29 16:54:11 +0200

    [add] bonus 1

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,34 +1,55 @@
 
-const { createApp } = Vue
+const { createApp } = Vue;
 
 createApp({
-    data() {
-      return {
-        movie: {
-          image: [
-            "./assets/img/01.webp",
-            "./assets/img/02.webp",
-            "./assets/img/03.webp",
-            "./assets/img/04.webp",
-            "./assets/img/05.webp",
-          ],
-        },
-        activeImage: 0,
-      };
-    },
-    methods: {
-      next() {
-        this.activeImage++;
-        if (this.activeImage === this.movie.image.length) {
-          this.activeImage = 0;
-        }
+  data() {
+    return {
+      movie: {
+        image: [
+          {
+            src: "./assets/img/01.webp",
+            title: "Spiderman",
+            description: "Lorem ipsum"
+          },
+          {
+            src: "./assets/img/02.webp",
+            title: "Non so chi siano",
+            description: "Lorem ipsum perplesso",
+          },
+          {
+            src: "./assets/img/03.webp",
+            title: "Fortnite",
+            description: "Giochino dove costruisci fortini e ammazzi gente",
+          },
+          {
+            src: "./assets/img/04.webp",
+            title: "Stray",
+            description: "Gattino a zonzo",
+          },
+          {
+            src: "./assets/img/05.webp",
+            title: "Avangers",
+            description: "Supereroi che salvano il mondo"
+          },
+        ],
       },
-      prev() {
-        this.activeImage--;
-        if (this.activeImage < 0) {
-          this.activeImage = this.movie.image.length - 1;
-        }
-      },
+      activeImage: 0,
+    };
+  },
+  methods: {
+    next() {
+      this.activeImage++;
+      if (this.activeImage === this.movie.image.length) {
+        this.activeImage = 0;
+      }
     },
-  }).mount("#app");
+    prev() {
+      this.activeImage--;
+      if (this.activeImage < 0) {
+        this.activeImage = this.movie.image.length - 1;
+      }
+    },
+  },
+}).mount("#app");
+
   
